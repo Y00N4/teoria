bb7d39093c168c59e54fcb8f81974cdbf5bb0c29
Y00N4 <126967988+Y00N4@users.noreply.github.com>
2023-03-27 20:47:37 +0200

    Update and rename struktura.cpp to mapa.cpp

---
+++ mapa.cpp
@@ -0,0 +1,127 @@
+#include<iostream>
+#include<map>
+#include<vector>
+
+using namespace std;
+
+struct ble {
+    int a;
+    int b;
+    string blee;
+    friend ostream& operator<<(ostream& os, const ble& ble);
+};
+
+ostream& operator<<(ostream& os, const ble& ble)
+{
+    os << ble.a << "-" << ble.b << "-" << ble.blee;
+    return os;
+}
+
+
+
+void mapsIntroduction()
+{
+    map<char, string> nameMap{ {'Z', "Zuzia"}, { 'D', "Damian" } };
+
+    nameMap.insert(pair<char, string> {'A', "Agnieszka"});
+    nameMap.insert(make_pair('A', "Agnieszka"));
+    nameMap['K'] = "Krzysztof";
+    nameMap.insert({ 'R', "Rekin" });
+
+    for (pair<char, string> el : nameMap)
+    {
+        cout << el.first << ": " << el.second << endl;
+    }
+
+    for (auto el : nameMap)
+    {
+        cout << el.first << ": " << el.second << endl;
+    }
+
+    cout << "\n\n";
+
+    nameMap.insert(pair<char, string> {'A', "Aga"});
+    nameMap.insert({ 'R', "Rekinek" });
+
+    for (pair<char, string> el : nameMap)
+    {
+        cout << el.first << ": " << el.second << endl;
+    }
+
+    cout << "\n\n";
+
+    nameMap.insert_or_assign('A', "Aga");
+    nameMap.insert_or_assign('R', "Rekinek");//podmienianie
+
+    for (pair<char, string> el : nameMap)
+    {
+        cout << el.first << ": " << el.second << endl;
+    }
+
+    cout << "\n\n";
+
+    cout << nameMap.size() << endl; //zwraca ilość el.
+    cout << nameMap.empty() << endl; //0 - false 1 - true - bool czy mapa pusta
+
+    //nameMap.emplace('I', "Ifif");//innyb sposob na dodawanie
+    //nameMap.try_emplace('M', "Mis");// tu proba zamiany nie dziala 
+
+
+    nameMap.erase('R');
+
+    if (nameMap.find('P') != nameMap.end()) {
+        cout << "Jest";
+    }
+    else
+        cout << "Nie ma";
+
+    cout << "\n";
+
+    nameMap.clear();
+
+    cout << "MAPA:\n";
+
+
+    for (pair<char, string> el : nameMap)
+    {
+        cout << el.first << ": " << el.second << endl;
+    }
+
+
+    cout << endl << endl;
+}
+void advancedMaps() {
+    map<int, pair<string, pair<ble, ble>>> mapka;
+
+    ble ble1{ 1,1,"ble" }, ble2{ 2,2,"blee" }, ble3{ 3,3,"bleee" };
+
+
+
+    mapka.insert({ 1, {"Zuzia",{ble1, ble2}} });
+    mapka.insert({ 2, {"Zuzia",{ble3, ble2}} });
+    mapka.insert({ 3, {"Zuzia",{ble1, ble3}} });
+
+    for (auto el : mapka) {
+        cout << el.first << ": " << el.second.first << " " << el.second.second.first << " " << el.second.second.second << endl;
+    }
+}
+
+
+int main() {
+
+    string slowo = "indywidualizacja";
+    map<char, int> licznik;
+
+    for (char lit : slowo) {
+        if (licznik.find(lit) != licznik.end()) {
+            licznik[lit]++;
+        }
+        else
+            licznik.insert({lit, 1});
+    }
+    
+
+
+    for (auto el : licznik) {
+        cout << el.first << ": " << el.second << endl;
+    }
