5bb1ad450cd35c09b183c92a9c1d9bc2b29bfbb8
Y00N4 <126967988+Y00N4@users.noreply.github.com>
2023-04-03 20:51:57 +0200

    Update mapa.cpp

--- mapa.cpp
+++ mapa.cpp
@@ -125,3 +125,28 @@ int main() {
     for (auto el : licznik) {
         cout << el.first << ": " << el.second << endl;
     }
+
+// VECTOR
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // definicja wektora liczb całkowitych
+    vector<int> v;
+
+    // dodanie elementów do wektora
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+
+    // wyświetlenie elementów wektora
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
+
